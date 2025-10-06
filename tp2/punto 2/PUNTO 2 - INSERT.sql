13b4ae0d8dadd25947ac00cc4cd5c98a37583e13
Santiago De Dios <thomassantiago005@gmail.com>
2025-10-05 23:37:04 -0300

    Update PUNTO 2 - INSERT.sql

--- tp2/punto 2/PUNTO 2 - INSERT.sql
+++ tp2/punto 2/PUNTO 2 - INSERT.sql
@@ -9,18 +9,18 @@ VALUES
 INSERT INTO Asuntos (numero_expediente, dni_cliente, fecha_inicio, fecha_fin, estado)
 VALUES
 (1, '123456789', '2023-01-15', '2023-07-20', 'Cerrado'),
-(2, '987654321', '2023-05-10', NULL, 'Abierto'),
+(2, '987654321', '2023-05-10', NULL, 'Abierto');
 (3, '456123789', '2023-06-01', '2023-09-10', 'Cerrado');
 
 -- Poblar la tabla Procuradores
-INSERT INTO Procuradores (id_procurador, nombre, direccion)
+INSERT INTO Procuradores (dni_procurador, nombre, direccion)
 VALUES
 (1, 'Laura Sánchez', 'Calle Soler 3765, Buenos Aires'),
 (2, 'Carlos López', 'Calle Estrellas 8, Mar del Plata'),
 (3, 'Marta Díaz', 'Calle Estación 12, Olavarria');
 
 -- Poblar la tabla Asuntos_Procuradores
-INSERT INTO Asuntos_Procuradores (numero_expediente, id_procurador)
+INSERT INTO Asuntos_Procuradores (numero_expediente, dni_del_procurador)
 VALUES
 (1, 1),
 (2, 2),
